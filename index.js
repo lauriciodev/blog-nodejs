b1869c51ee1b6bada0db566e22da2cf8031f391c
LauricioX <developerwebmasterpro@gmail.com>
2022-10-04 09:24:45 -0300

    config

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//set view engine
+app.set("view engine","ejs");
+
+//static
+app.use(express.static("public"));
+
+//set body-parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+
+
+app.get("/",(req,res) =>{
+  res.render("index");
+});
+
+app.listen(8080, () =>{
+  console.log("servidor ok!")
+})
