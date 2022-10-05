b6376af8d8ad216a719d185466736c99835673ca
LauricioX <developerwebmasterpro@gmail.com>
2022-10-05 16:03:45 -0300

    formulario de artigos

--- articles/articlesControles.js
+++ articles/articlesControles.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const categorieModel = require('../categories/categoriesModel');
 const router = express.Router();
+const categoriesModel = require("../categories/categoriesModel");
+const articlesModel = require("../articles/articlesModel");
+const slugify = require("slugify");
 
-router.get("/articles",(req,res) =>{
+router.get("/admin/articles",(req,res) =>{
   res.send("rota de artigos")
 });
 
 router.get("/admin/articles/new",(req,res) =>{
-  res.render("admin/articles/new");
+  categorieModel.findAll().then( categories =>{
+    res.render("admin/articles/new",{
+      categories:categories
+    });
+  })
+});
+
+router.post("/articles/save",(req,res)=>{
+  let title = req.body.title;
+  let body = req.body.body;
+  let categories = req.body.categories;
+
+  articlesModel.create({
+    title:title,
+    slug:slugify(title),
+    body:body,
+    categoryId:categories
+  }).then(() =>{
+    res.redirect("/admin/articles")
+  })
+
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("blog-project","root","berlim2062",{
   host:"localhost",
-  dialect:"mysql"
+  dialect:"mysql",
+  timezone:"-03:00"
 });
 
 module.exports = connection;
