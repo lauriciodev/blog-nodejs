a5357895c6193fcbf4805f2bb54e7bba5678a9b1
LauricioX <developerwebmasterpro@gmail.com>
2022-10-12 10:37:28 -0300

    dd

--- articles/articlesControles.js
+++ articles/articlesControles.js
@@ -103,9 +103,41 @@ router.post("/articles/update", (req,res) =>{
 
 router.get("/articles/page/:num",(req,res) =>{
   let page = req.params.num;
+  let offset = 0
 
-  articlesModel.findAndCountAll().then(articles =>{
-    res.json(articles);
+ if(isNaN(page) || page == 1){
+  offset = 0
+
+ }else{
+  offset = parseInt(page) * 4;
+ }
+ 
+
+  articlesModel.findAndCountAll({
+    limit:4,
+    offset:offset
+  }).then(articles =>{
+
+    let next;
+
+    if(offset + 4 >= articles.count){
+      next = false;
+    }else{
+      next = true;
+    }
+  
+    let result = {
+      next:next,
+      articles:articles
+    }
+
+
+    res.json(result);
   })
-})
+});
+
+
+
+
+
 module.exports = router;
