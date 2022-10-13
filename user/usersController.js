966e1e7ad53337191f944df77b4cd90e4e5278aa
LauricioX <developerwebmasterpro@gmail.com>
2022-10-12 23:08:12 -0300

    envio de dados usuario

--- user/usersController.js
+++ user/usersController.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const router = express.Router();
-const User = require("./user");
+const userModel = require("./user");
 
 router.get("/admin/users",(req,res) =>{
   res.send("listagemde usuarios");
@@ -8,7 +8,24 @@ router.get("/admin/users",(req,res) =>{
 
 
 router.get("/admin/users/create",(req,res) =>{
-  res.render("admin/user/create")
+  res.render("admin/users/create")
+})
+
+router.post("/users/create", (req,res) =>{
+  let email = req.body.email;
+  let password = req.body.password;
+
+
+  res.json({
+    email,
+    password
+  })
+
+ /*  userModel.create({
+    email:email,
+    password:password
+  })
+ */
 })
 
 
